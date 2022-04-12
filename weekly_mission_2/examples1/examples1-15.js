5c1c7aed749eec7739b38d08e2024ad2dc0d1211
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-12 16:25:33 -0700

    se agrego el ejemplo 15 junto con comentarios y dos lineas de codigo mas

---
+++ weekly_mission_2/examples1/examples1-15.js
@@ -0,0 +1,6 @@
+//Ejemplo 15: Uso de sort para ordenar elementos
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
+const numbers15 = [2, 34, 122, 4, 1, 10]
+console.log("Ejemplo 15: Elementos ordernados usando SORT")
+console.log(products.sort()) //dado que son solo strings los ordena alfabeticamente
+console.log(numbers15.sort()); //los acomoda solo considerando el primer numero
