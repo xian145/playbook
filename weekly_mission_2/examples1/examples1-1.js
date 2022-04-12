d1c4fb37da239a77e2351254d91b0a1c0ef3bb05
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-12 15:15:52 -0700

    Se agrego el ejemplo 1 junto con comentarios

---
+++ weekly_mission_2/examples1/examples1-1.js
@@ -0,0 +1,5 @@
+// Ejemplo 1: for Each para imprimir elementos de una lista
+const numbers = [1, 2, 3, 4, 5];
+console.log("Ejemplo 1: Imprimiendo los elementos de una lista")
+numbers.forEach(num => console.log(num)) //lo que hace basicamente es crear un ciclo por cada elemento que se encunetre en la lista, razon por la cual se llama forEach
+//num es una variable creada solo para el ciclo el cual se cambiara su valor por cada elemento dentro de la lista
