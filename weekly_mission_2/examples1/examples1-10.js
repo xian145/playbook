e6cacfcd588cb441dcbe14ebdd31c1d750209e4d
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-12 16:08:24 -0700

    se agrego el ejemplo 10 junto con comentarios para el mejor entendimiento del codigo

---
+++ weekly_mission_2/examples1/examples1-10.js
@@ -0,0 +1,4 @@
+// Ejemplo 10: uso de every nos ayuda a validar todos los elementos de una lista, si todos cumplen con la validación que indiques te regresa true, de lo contrario false
+const names10 = ['Explorer 1', 'Explorer 2', 'Explorer 3', 'Explorer 4']
+const areAllStr = names10.every((name) => typeof name === 'string') //every nos dara True, en caso de que almenos uno de los ed la lista no cumpla saldra como False pero no nos dira que elemento es
+console.log("Ejemplo 10: Son todos los nombres string: " + areAllStr) //en la lista de names10 checa todos (every) los elementos y fijate si el tipo de variable (typeof) sting
