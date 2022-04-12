99c5d7daf3b5efdfc993e4ac4f79b315ffe9c9a4
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-12 14:39:19 -0700

    creando el ejemplo de objeto vacio y agregando un poco de comentarios

---
+++ weekly_mission_2/examples0/examples0-1.js
@@ -0,0 +1,5 @@
+//Ejemplo 1
+
+const myObject = {} //aqui se creo un objeto vacio, es posible agregar cosas con metodos
+console.log('Ejemplo 1: Crear un objeto vacio');
+console.log(myObject); //no debera de imprimir nada mas que las llaves, ya que el obejto esta vacio
