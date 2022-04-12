4cd2115c13610361537111b238eda3a94335832e
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-12 14:47:18 -0700

    Se agrego el ejemplo 2 con algunos comentarios

---
+++ weekly_mission_2/examples0/examples0-2.js
@@ -0,0 +1,11 @@
+//Ejemplo 2
+
+const myObject2 = {
+    name: "Abraham",
+    lastName: "Arizmendi Grijalva",
+    age: 29
+}
+
+console.log("Ejemplo 2: Crear un objeto con propiedades");
+console.log(myObject2); //saldra todas las propiedades dentro del objeto, se puede seleccionar solo una de ellas para mostrar si se desea
+console.log(`La edad de la persona es: ${myObject2.age}`);
