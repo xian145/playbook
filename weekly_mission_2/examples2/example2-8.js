c6148ec56ad4e764eb94cc4ad132b984002c5383
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-13 10:32:43 -0700

    se creo el archivo con el ejemplo 8 donde se ve el metodo static con el cual podemos definir una clase con cierto metodos ya predefinidos

---
+++ weekly_mission_2/examples2/example2-8.js
@@ -0,0 +1,14 @@
+// Ejemplo 8: Métodos static nos ayudan a escribir métodos en una clase que podemos usar sin necesidad de instanciar algún objeto
+class Toolbox {
+    static getMostUsefulTools(){ //lo que hace es algo asi como los valores default pero con metodo de una clase, de esta manera no tenemos que instanciar/crear la clase, ya que tiene valor predefinidos
+        return ["Command line", "git", "Text Editor"] //metodos de la clase
+    }
+}
+  
+console.log("Ejemplo 8: Métodos static")
+// Puedo llamar el método static directamente con el nombre de la clase
+console.log(Toolbox.getMostUsefulTools())
+console.log(Toolbox); //solo nos dira que es una clase con el nombre de toolbox
+// Si intentamos instanciar un objeto, no podremos llamar este método static
+//const toolbox = new Toolbox()
+//console.log(toolbox.getMostUsefulTools()) // is not a function
