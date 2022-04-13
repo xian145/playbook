0c713dddf69a0e67adf1a16d285de0c88b30b50d
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-13 09:50:12 -0700

    se creo el archivo para el ejemplo 1 donde se crea una clase vacia

---
+++ weekly_mission_2/examples2/example2-1.js
@@ -0,0 +1,5 @@
+// Ejemplo 1: Crear una clase vacía
+class Person {
+} //clase vacia, se define parecido a los objetos
+console.log("Ejemplo 1: Crear una clase vacía")
+console.log(Person) // [class Person] lo que se imprime en terminal
