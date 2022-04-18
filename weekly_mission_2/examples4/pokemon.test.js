6b1da4d5e421d0873f761f372599574cf1042f17
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-18 12:29:54 -0700

    se agregaron dos pruebas mas referentes al tipo y a la edad del pokemon

--- weekly_mission_2/examples4/pokemon.test.js
+++ weekly_mission_2/examples4/pokemon.test.js
@@ -1,6 +1,8 @@
 import Pokemon from './pokemon'
 
 test('1) Create a new object pokemon', () => {
-    const myPokemon = new Pokemon('Pikachu')
+    const myPokemon = new Pokemon('Pikachu', "Electric", 2)
     expect(myPokemon.name).toBe('Pikachu'); //la primera parte nos indica lo que esta recibiendo, mientras que el toBe es lo que espera recibir, en caso de no ser asi, nuetra pureba fallara
+    expect(myPokemon.type).toBe("Electric") //distingue entre mayusculas y minusculas
+    expect(myPokemon.age).toBe(2) 
 });
