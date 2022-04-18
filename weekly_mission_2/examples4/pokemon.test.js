946e3a0fef417761e3297cdef8a0c5ee7a6141dd
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-18 12:19:11 -0700

    se modificio la prueba para que se validara

--- weekly_mission_2/examples4/pokemon.test.js
+++ weekly_mission_2/examples4/pokemon.test.js
@@ -2,5 +2,5 @@ import Pokemon from './pokemon'
 
 test('1) Create a new object pokemon', () => {
     const myPokemon = new Pokemon('Pikachu')
-    expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba
+    expect(myPokemon.name).toBe('Pikachu'); //la primera parte nos indica lo que esta recibiendo, mientras que el toBe es lo que espera recibir, en caso de no ser asi, nuetra pureba fallara
 });
